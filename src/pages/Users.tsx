384300a937cf22c40ecc02a6d91fe5e800126cc8
mcfilu <filip.1olszewski@gmail.com>
2024-02-26 18:29:41 +0000

    adding the code for fetching and displaying the data

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import UserWelcome from '../components/users/UserWelcome';
+import UserDetails from '../components/users/UserDetails';
 
 const Users: React.FC = () => {
+    const { id } = useParams<{ id: string }>();
     return (
       
-        <div>
-          User
+        <div className='w-screen h-screen bg-gradient-to-tr from-zinc-100 via-black to-zinc-100 grid grid-cols-2 justify-center items-center p-[10vh] gap-[4vh]'>
+            <UserWelcome/>
+            <UserDetails id_val={id}/>
+            
         </div>
       
     );
