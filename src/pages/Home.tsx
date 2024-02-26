63d62a39ea08423ad36289870b12996e7345d3a7
mcfilu <filip.1olszewski@gmail.com>
2024-02-26 17:25:38 +0000

    initialising tailwindcss configuration, tested, ready for the development

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,8 +3,10 @@ import React from 'react';
 const Home = () => {
     return (
       
-        <div>
-          Home
+        <div className='w-screen h-screen bg- flex'>
+          <div className='w-1/2 h-1/2'>
+            <h1 className='text-black text-[3vh]'>Welcome to the Pulse.io Front-End Application</h1>
+          </div>
         </div>
 
       
