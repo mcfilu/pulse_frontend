0dbbe8d4fb45b04ddb383e3a08d43bf49e38aad1
mcfilu <filip.1olszewski@gmail.com>
2024-02-26 17:21:02 +0000

    initalising the project with create vite@latest, plus setting up the project structure and routing

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Users from './pages/Users'
+import './App.css'
+
+
+function App() {
+
+
+  return (
+    <>
+      <Routes>
+        
+        <Route path="/" element={<Home/>} />
+        <Route path="/users/:id" element={<Users/>} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/Users.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Users: React.FC = () => {
+    return (
+      
+        <div>
+          User
+        </div>
+      
+    );
+  }
+
+export default Users
