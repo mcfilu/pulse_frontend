856ceeb5a8fde44268c24efdf028fd554c88feca
mcfilu <filip.1olszewski@gmail.com>
2024-02-27 15:45:43 +0000

    adding the next test to Users page for the negative integer param

--- src/pages/__tests__/Users.integration.test.tsx
+++ src/pages/__tests__/Users.integration.test.tsx
@@ -14,6 +14,15 @@ describe('Users component', () => {
 
     expect(screen.getByText('Wrong type of the parameter passed to the url. Make sure you put the positive integer.')).toBeInTheDocument();
   });
+  it('should render UserError component with error message for "-1" param', () => {
+    render(
+      <MemoryRouter initialEntries={['/-1']}>
+        <Users />
+      </MemoryRouter>
+    );
+
+    expect(screen.getByText('Wrong type of the parameter passed to the url. Make sure you put the positive integer.')).toBeInTheDocument();
+  });
 
   
 });
