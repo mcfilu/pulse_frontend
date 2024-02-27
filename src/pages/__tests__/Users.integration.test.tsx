5708ad69c436d830ba34cfff1a3d7e7548ab9895
mcfilu <filip.1olszewski@gmail.com>
2024-02-27 16:27:17 +0000

    adding the next test case for the Users page, tests the whole page with positive scenario when api delivers the values and checks whether they are being displayed correctly

--- src/pages/__tests__/Users.integration.test.tsx
+++ src/pages/__tests__/Users.integration.test.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom'
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, Routes, Route } from 'react-router-dom';
 import Users from '../Users';
 
 describe('Users component', () => {
   it('should render UserError component with error message for "abc" param', () => {
     render(
-      <MemoryRouter initialEntries={['/abc']}>
-        <Users />
+    <MemoryRouter initialEntries={['/abc']}>
+        <Routes>
+            <Route path="/:id" element={<Users/>}/>
+        </Routes>
       </MemoryRouter>
     );
 
@@ -16,40 +18,61 @@ describe('Users component', () => {
   });
   it('should render UserError component with error message for "-1" param', () => {
     render(
-      <MemoryRouter initialEntries={['/-1']}>
-        <Users />
+    <MemoryRouter initialEntries={['/-1']}>
+        <Routes>
+            <Route path="/:id" element={<Users/>}/>
+        </Routes>
       </MemoryRouter>
     );
 
     expect(screen.getByText('Wrong type of the parameter passed to the url. Make sure you put the positive integer.')).toBeInTheDocument();
   });
-//   it('should render UserDetails component with correct values for "3" param', async () => {
-//     // Mock the fetch call
-//     const mockUserData = {
-//         id: 1,
-//         avatar: 'https://reqres.in/img/faces/1-image.jpg',
-//         email: 'george.bluth@reqres.in',
-//         first_name: 'George',
-//         last_name: 'Bluth',
-//       };
+
+    it('should render UserError component when API call fails', async () => {
+    global.fetch = jest.fn().mockRejectedValueOnce(new Error('Failed to fetch'));
   
-//       global.fetch = jest.fn().mockResolvedValue({
-//         ok: true,
-//         json: async () => mockUserData,
-//       });
-
-//     render(
-//       <MemoryRouter initialEntries={['/1']}>
-//         <Users />
-//       </MemoryRouter>
-//     );
-
-//     // Wait for UserDetails component to render
-//     await waitFor(() => {
-//       expect(screen.getByText('User id: 1')).toBeInTheDocument();
-//       // Add assertions for other user information if needed
-//     });
-//   });
+    render(
+      <MemoryRouter initialEntries={['/1']}>
+        <Routes>
+        <Route path="/:id" element={<Users/>}/>
+        </Routes>
+      </MemoryRouter>
+    );
+  
+    await waitFor(() => {
+      expect(screen.getByText('There is an error coming from the back-end response.')).toBeInTheDocument();
+    });
+  });
+  
+  it('should render UserDetails component with corresponding user details when API call is successful', async () => {
+    const mockUserData = {
+      id: 1,
+      avatar: 'https://reqres.in/img/faces/1-image.jpg',
+      email: 'george.bluth@reqres.in',
+      first_name: 'George',
+      last_name: 'Bluth',
+    };
+  
+    global.fetch = jest.fn().mockResolvedValueOnce({
+      ok: true,
+      json: async () => mockUserData,
+    });
+  
+    render(
+      <MemoryRouter initialEntries={['/1']}>
+        <Routes>
+          <Route path="/:id" element={<Users />} />
+        </Routes>
+      </MemoryRouter>
+    );
+  
+    await waitFor(() => {
+      expect(screen.getByText('User id: 1')).toBeInTheDocument();
+      expect(screen.getByText('Email: george.bluth@reqres.in')).toBeInTheDocument();
+      expect(screen.getByText('First Name: George')).toBeInTheDocument();
+      expect(screen.getByText('Second Name: Bluth')).toBeInTheDocument();
+    });
+  });
 
   
 });
